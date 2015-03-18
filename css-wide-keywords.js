8d6c241faa1246137a57b9f3cefcafbf30f14966
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-03-18 12:38:26 +0900

    [ci skip] temporary commit (tmp branch)

--- css-wide-keywords.js
+++
@@ -1,8 +0,0 @@
-/*!
- * css-wide-keywords | MIT (c) Shinnosuke Watanabe
- * https://github.com/shinnn/css-wide-keywords
-*/
-(function() {
-  'use strict';
-  window.cssWideKeywords = ['initial', 'inherit'];
-}());
